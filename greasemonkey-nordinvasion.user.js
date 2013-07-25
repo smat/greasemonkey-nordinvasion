8c9bb5e0ce0812dfafe4da613758346611529c72
Stian Mathiassen <stian.mathiassen@bekk.no>
2013-07-25 18:52:49 +0200

    Include jQuery, since Chrome extension are run in own sandbox

--- greasemonkey-nordinvasion.user.js
+++ greasemonkey-nordinvasion.user.js
@@ -3,25 +3,21 @@
 // @namespace  http://github.com/smat
 // @version    0.1
 // @description  Shows the price per item on the NordInvasion auction hall
-// @match      http://nordinvasion.com/*
-// @match      https://nordinvasion.com/*
+// @match      http://*.nordinvasion.com/*
+// @match      https://*.nordinvasion.com/*
 // @grant      none
 // @copyright  2012+, Stian Mathissen
 // ==/UserScript==
 
-$(document).ready(function() {
+$('.ah-item-row').each(function(index, el) {
+    jel = $(el);
+    quantityRegexMatch = jel.text().match(/^\s*(\d+) x/);
+    amountRegexMatch = jel.find('span:eq(2)').text().match(/^\s*(\d+)/);
 
-    $('.ah-item-row').each(function(index, el) {
-        jel = $(el);
-        quantityRegexMatch = jel.text().match(/^\s*(\d+) x/);
-        amountRegexMatch = jel.find('span:eq(2)').text().match(/^\s*(\d+)/);
+    quantity = quantityRegexMatch[1];
+    amount = amountRegexMatch[1];
 
-        quantity = quantityRegexMatch[1];
-        amount = amountRegexMatch[1];
+    pricePer = amount / quantity;
 
-        pricePer = amount / quantity;
-
-        jel.append($('<span>').css('display', 'block').css('float', 'right').css('width', '100px').text(pricePer.toFixed(1) + " per"));
-    });
-
+    jel.append($('<span>').css('display', 'block').css('float', 'right').css('width', '100px').text(pricePer.toFixed(1) + " per"));
 });
